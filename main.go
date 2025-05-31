3f1d66061f1126e2c6cc6e13357d7be671d2c76e
Ghazanfar <ali.ghazanfar.dev@gmail.com>
2025-05-31 21:39:22 +0200

    (Feat)-Update API implemented

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -59,9 +61,34 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
+	//set json content type
+	w.Header().Set("Content-Type", "application/json")
+	//params
+	params := mux.Vars(r)
+	//loop over the movies
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// delete the movie with id received
+			movies = append(movies[:index], movies[index+1:]...)
+			// add a new movie - the movie we sent in the body
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			return
+
+		}
+	}
 
 }
 
